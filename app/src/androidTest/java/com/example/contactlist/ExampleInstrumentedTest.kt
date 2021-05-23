1ff51ffca33142df09f8b9c7e17cca0845195337
Abiola Olatunbosun <olaide_abiola87@yahoo.com>
2021-05-23 14:04:08 +0100

    Initial Commit

--- app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/contactlist/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.android.zuritask.nsikak.contactlist
+package com.example.contactlist
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.android.zuritask.nsikak.com.example.contactlist", appContext.packageName)
+        assertEquals("com.example.contactlist", appContext.packageName)
     }
 }
